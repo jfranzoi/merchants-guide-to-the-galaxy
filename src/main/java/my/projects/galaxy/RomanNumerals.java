72c7afb322c6b31817eb80ce0bc48fb28576aa4e
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2020-07-05 13:51:16 +0200

    numerals, roman: refactored on combined simple

--- src/main/java/my/projects/galaxy/RomanNumerals.java
+++ src/main/java/my/projects/galaxy/RomanNumerals.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class RomanNumerals implements Numerals {
 
-  private static HashMap<String, Long> DEFAULTS = new HashMap<String, Long>() {
+  private static HashMap<String, Long> VALUES = new HashMap<String, Long>() {
     {
       put("I", 1L);
       put("V", 5L);
       put("X", 10L);
       put("L", 50L);
+      put("C", 100L);
+      put("D", 500L);
+      put("M", 1000L);
     }
   };
 
@@ -22,11 +25,11 @@ public class RomanNumerals implements Numerals {
   }
 
   private Long compute(String symbol) {
-    return DEFAULTS.get(symbol);
+    return VALUES.get(symbol);
   }
 
-  private Long add(Long one, Long another) {
-    return one + another;
+  private Long add(Long first, Long second) {
+    return first + second;
   }
 
 }
--- src/test/java/my/projects/galaxy/RomanNumeralsTest.java
+++ src/test/java/my/projects/galaxy/RomanNumeralsTest.java
@@ -5,34 +5,35 @@ import static org.hamcrest.Matchers.is;
 
 import java.util.Arrays;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RomanNumeralsTest {
 
+  private RomanNumerals numerals = new RomanNumerals();
+
   @Test
-  public void defaultSymbols_I() {
-    assertThat(new RomanNumerals().compute(Arrays.asList("I")), is(1L));
+  public void singleSymbols() {
+    assertThat(numerals.compute(Arrays.asList("I")), is(1L));
+    assertThat(numerals.compute(Arrays.asList("V")), is(5L));
+    assertThat(numerals.compute(Arrays.asList("X")), is(10L));
+    assertThat(numerals.compute(Arrays.asList("L")), is(50L));
+    assertThat(numerals.compute(Arrays.asList("C")), is(100L));
+    assertThat(numerals.compute(Arrays.asList("D")), is(500L));
+    assertThat(numerals.compute(Arrays.asList("M")), is(1000L));
   }
 
   @Test
-  public void defaultSymbols_V() {
-    assertThat(new RomanNumerals().compute(Arrays.asList("V")), is(5L));
+  public void multipleSymbolsAdded() throws Exception {
+    assertThat(numerals.compute(Arrays.asList("V", "I")), is(6L));
+    assertThat(numerals.compute(Arrays.asList("X", "V", "I")), is(16L));
   }
 
   @Test
-  public void defaultSymbols_X() {
-    assertThat(new RomanNumerals().compute(Arrays.asList("X")), is(10L));
-  }
-
-  @Test
-  public void defaultSymbols_L() {
-    assertThat(new RomanNumerals().compute(Arrays.asList("L")), is(50L));
-  }
-
-  @Test
-  public void combineSimple() throws Exception {
-    assertThat(new RomanNumerals().compute(Arrays.asList("V", "I")), is(6L));
-    assertThat(new RomanNumerals().compute(Arrays.asList("X", "V", "I")), is(16L));
+  @Ignore("WIP")
+  public void multipleSymbolsSubtracted() throws Exception {
+    assertThat(numerals.compute(Arrays.asList("X", "L", "I", "I")), is(42L));
+    assertThat(numerals.compute(Arrays.asList("M", "C", "M", "X", "L", "I", "V")), is(1944L));
   }
 
 }
